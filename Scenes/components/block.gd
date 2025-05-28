f7e1e293f6729032db2f86dc6a17e5cddbb715e6
Rico Apon <rico.apon@gmail.com>
2025-05-28 19:17:42 +0200

    Add creation of lines between blocks

--- Scenes/components/block.gd
+++ Scenes/components/block.gd
@@ -1,4 +1,8 @@
-extends Node2D
+# We use a ColorRect because:
+# 1. We can easily catch mouse events on the sprite (which doesn't have a click event, but ColorRect does).
+# 2. We can scale the size of the ColorRect and Sprite2D will automatically scale with it.
+# 3. For debugging, we can give it a color (instead of transparent) so we can see what is happening.
+extends ColorRect
 class_name Block
 
 const ACTUAL_SIZE: float = 64
@@ -6,14 +10,11 @@ const ACTUAL_SIZE: float = 64
 @export var grid_pos: Vector2i
 
 func _ready():
-	# The variable grid_pos will give us the position of the top left of the square
-	# we want to land it. We want to move to the middle of that square.
-	position = Vector2(0.5 + grid_pos.x, 0.5 + grid_pos.y) * grid_cell_size
-	
-	$ColorRect.scale = Vector2(ACTUAL_SIZE / grid_cell_size, ACTUAL_SIZE / grid_cell_size)
-	set_sprite_size($Sprite2D, Vector2(grid_cell_size * 0.75, grid_cell_size * 0.75))
+	position = Vector2(grid_pos.x, grid_pos.y) * grid_cell_size
+	scale = Vector2(grid_cell_size / ACTUAL_SIZE, grid_cell_size / ACTUAL_SIZE)
 
-func set_sprite_size(sprite: Sprite2D, desired_size: Vector2):
-	if sprite.texture:
-		var texture_size = sprite.texture.get_size()
-		sprite.scale = desired_size / texture_size
+signal drag_start(grid_position: Vector2i)
+
+func _gui_input(event: InputEvent):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		drag_start.emit(grid_pos)
--- Scenes/components/grid.gd
+++ Scenes/components/grid.gd
@@ -38,6 +38,8 @@ func place_block(pos: Vector2i):
 	block.grid_cell_size = CELL_SIZE
 	add_child(block)
 	blocks[pos] = block
+	block.connect("drag_start", on_start_block_drag)
+	block.z_index = 2
 
 func _can_drop_data(_at_position, data):
 	return data == "BlockType:Basic"
@@ -46,3 +48,33 @@ func _drop_data(at_position, _data):
 	var x = floor(float(at_position.x) / CELL_SIZE)
 	var y = floor(float(at_position.y) / CELL_SIZE)
 	place_block(Vector2i(x, y))
+
+
+var start_drag_grid_position = null
+
+func on_start_block_drag(grid_position: Vector2i):
+	start_drag_grid_position = grid_position
+
+func _input(event):
+	# If dragging:
+	if start_drag_grid_position != null and event is InputEventMouseButton and not event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		var mouse_pos = get_global_mouse_position()
+
+		# Find block under mouse
+		for pos in blocks.keys():
+			var block = blocks[pos]
+			if block.get_global_rect().has_point(mouse_pos):
+				add_line(start_drag_grid_position, pos)
+				break
+		start_drag_grid_position = null
+
+func add_line(from_grid_pos: Vector2i, to_grid_pos: Vector2i):
+	var line = Line2D.new()
+	line.z_index = 1
+	line.default_color = Color.RED
+	line.width = 4
+	var from_middle = (Vector2(from_grid_pos.x + 0.5, from_grid_pos.y + 0.5)) * CELL_SIZE
+	var to_middle = (Vector2(to_grid_pos.x + 0.5, to_grid_pos.y + 0.5)) * CELL_SIZE
+	line.add_point(from_middle)
+	line.add_point(to_middle)
+	add_child(line)
