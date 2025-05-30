620d83b1358ebe2a8e5a0a969db49eff1c4b32eb
Rico Apon <rico.apon@gmail.com>
2025-05-30 09:57:41 +0200

    Fix length when from is null

--- scripts/logic/edges/PoweredEdge.gd
+++ scripts/logic/edges/PoweredEdge.gd
@@ -13,6 +13,11 @@ func _init(_from: Coordinate, _to: Coordinate, _power: int):
 	from = _from
 	to = _to
 	power = _power
-	# It shouldn't need rounding, but because distance_to is a float I thought it would be better to
-	# convert to an int like this.
-	length = round(Vector2i(from.x, from.y).distance_to(Vector2i(to.x, to.y)))
+	
+	# Starting edge has null, so we do it like this.
+	if from == null:
+		length = 1
+	else:
+		# It shouldn't need rounding, but because distance_to is a float I thought it would be better to
+		# convert to an int like this.
+		length = round(Vector2i(from.x, from.y).distance_to(Vector2i(to.x, to.y)))
