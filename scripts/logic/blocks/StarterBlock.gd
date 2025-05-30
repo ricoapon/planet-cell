1fc73e3057d24afbe9f2e887175d73209742c639
Rico Apon <rico.apon@gmail.com>
2025-05-30 09:57:22 +0200

    Fix typing issue

--- scripts/logic/blocks/StarterBlock.gd
+++ scripts/logic/blocks/StarterBlock.gd
@@ -1,13 +1,17 @@
 class_name StarterBlock
 extends AbstractBlock
 
-func activate(poweredEdge: PoweredEdge, out: Array[Edge]) -> Array[PoweredEdge]:
+func activate(powered_edge: PoweredEdge, out: Array[Edge]) -> Array[PoweredEdge]:
 	# If any power comes back, we stop. But this method is also called on the first activation, which
 	# should continue. We can differentiate these situations by checking if from is null. This should
 	# only happen on the first activation.
-	if poweredEdge.from != null:
+	if powered_edge.from != null:
 		return []
-	return out.map(func(e): return e.power(1))
+		
+	# To work around typing issues with map, we do this.
+	var result: Array[PoweredEdge]
+	result.assign(out.map(func(e): return e.to_powered_edge(1)))
+	return result
 
 func type() -> Type:
 	return Type.STARTER
