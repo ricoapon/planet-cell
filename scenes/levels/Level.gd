08f3b8f9d1d15b72b10e57754299680df717c6de
Rico Apon <rico.apon@gmail.com>
2025-06-07 14:56:54 +0200

    Add 5 introduction levels

--- scenes/levels/Level.gd
+++ scenes/levels/Level.gd
@@ -3,14 +3,31 @@ extends RefCounted
 
 static var all: Array[Level] = [
 	Level.new(
-		"Introduction 1",
-		"eyJjb29yZGluYXRlX3RvX2Jsb2NrIjp7IntcInhcIjoxLFwieVwiOjN9Ijp7InR5cGUiOjJ9LCJ7XCJ4XCI6MyxcInlcIjozfSI6eyJjb2xvciI6MCwidHlwZSI6MH19LCJlZGdlcyI6W3siZnJvbSI6eyJ4IjoxLCJ5IjozfSwidG8iOnsieCI6MywieSI6M319XSwiZXhwZWN0ZWRfb3V0cHV0Ijp7InJvd3MiOlt7IjAiOjEsIjEiOjAsIjIiOjB9XX0sImhlaWdodCI6MTAsIndpZHRoIjoxMH0="
+		# Requires player to only draw a line.
+		"Tutorial",
+		"eyJjb29yZGluYXRlX3RvX2Jsb2NrIjp7IntcInhcIjoxLFwieVwiOjR9Ijp7InR5cGUiOjJ9LCJ7XCJ4XCI6NSxcInlcIjo0fSI6eyJjb2xvciI6MCwidHlwZSI6MH19LCJlZGdlcyI6W10sImV4cGVjdGVkX291dHB1dCI6eyJyb3dzIjpbeyIwIjoxLCIxIjowLCIyIjowfV19LCJoZWlnaHQiOjEwLCJ3aWR0aCI6MTB9"
 	),
 	Level.new(
-		"Introduction 2",
-		"eyJjb29yZGluYXRlX3RvX2Jsb2NrIjp7IntcInhcIjowLFwieVwiOjl9Ijp7InR5cGUiOjJ9LCJ7XCJ4XCI6MixcInlcIjo5fSI6eyJjb2xvciI6MCwidHlwZSI6MH19LCJlZGdlcyI6W3siZnJvbSI6eyJ4IjowLCJ5Ijo5fSwidG8iOnsieCI6MiwieSI6OX19XSwiZXhwZWN0ZWRfb3V0cHV0Ijp7InJvd3MiOlt7IjAiOjEsIjEiOjAsIjIiOjB9XX0sImhlaWdodCI6MTAsIndpZHRoIjoxMH0="
+		# Requires player to place some blocks.
+		"Tutorial 2",
+		"eyJjb29yZGluYXRlX3RvX2Jsb2NrIjp7IntcInhcIjoxLFwieVwiOjN9Ijp7InR5cGUiOjJ9LCJ7XCJ4XCI6NCxcInlcIjo0fSI6eyJjb2xvciI6MCwidHlwZSI6MH19LCJlZGdlcyI6W10sImV4cGVjdGVkX291dHB1dCI6eyJyb3dzIjpbeyIwIjoyLCIxIjowLCIyIjowfV19LCJoZWlnaHQiOjEwLCJ3aWR0aCI6MTB9"
 	),
-	
+	Level.new(
+		# Still possible to achieve without splitting.
+		"Tutorial 3",
+		"eyJjb29yZGluYXRlX3RvX2Jsb2NrIjp7IntcInhcIjoxLFwieVwiOjN9Ijp7InR5cGUiOjJ9LCJ7XCJ4XCI6NCxcInlcIjo0fSI6eyJjb2xvciI6MCwidHlwZSI6MH19LCJlZGdlcyI6W10sImV4cGVjdGVkX291dHB1dCI6eyJyb3dzIjpbeyIwIjoyLCIxIjowLCIyIjowfV19LCJoZWlnaHQiOjEwLCJ3aWR0aCI6MTB9"
+	),
+	Level.new(
+		# The player should now learn how to split to increase power.
+		"Tutorial 4",
+		"eyJjb29yZGluYXRlX3RvX2Jsb2NrIjp7IntcInhcIjoxLFwieVwiOjR9Ijp7InR5cGUiOjJ9LCJ7XCJ4XCI6NSxcInlcIjo0fSI6eyJjb2xvciI6MCwidHlwZSI6MH19LCJlZGdlcyI6W10sImV4cGVjdGVkX291dHB1dCI6eyJyb3dzIjpbeyIwIjo0LCIxIjowLCIyIjowfV19LCJoZWlnaHQiOjEwLCJ3aWR0aCI6MTB9"
+	),
+	Level.new(
+		# Now we need some way to keep looping.
+		"Tutorial 5",
+		"eyJjb29yZGluYXRlX3RvX2Jsb2NrIjp7IntcInhcIjoxLFwieVwiOjF9Ijp7InR5cGUiOjJ9LCJ7XCJ4XCI6MyxcInlcIjo1fSI6eyJjb2xvciI6MCwidHlwZSI6MH19LCJlZGdlcyI6W10sImV4cGVjdGVkX291dHB1dCI6eyJyb3dzIjpbeyIwIjoyLCIxIjowLCIyIjowfSx7IjAiOjIsIjEiOjAsIjIiOjB9LHsiMCI6MiwiMSI6MCwiMiI6MH0seyIwIjoyLCIxIjowLCIyIjowfSx7IjAiOjIsIjEiOjAsIjIiOjB9XX0sImhlaWdodCI6MTAsIndpZHRoIjoxMH0"
+	),
+
 ]
 
 var title: String
