47dd85a99b182312ed37ca98f30bf4b33e856ee6
Rico Apon <rico.apon@gmail.com>
2025-06-07 19:24:29 +0200

    Fix level 2

--- scenes/levels/Level.gd
+++ scenes/levels/Level.gd
@@ -10,7 +10,7 @@ static var all: Array[Level] = [
 	Level.new(
 		# Requires player to place some blocks.
 		"Tutorial 2",
-		"eyJjb29yZGluYXRlX3RvX2Jsb2NrIjp7IntcInhcIjoxLFwieVwiOjN9Ijp7InR5cGUiOjJ9LCJ7XCJ4XCI6NCxcInlcIjo0fSI6eyJjb2xvciI6MCwidHlwZSI6MH19LCJlZGdlcyI6W10sImV4cGVjdGVkX291dHB1dCI6eyJyb3dzIjpbeyIwIjoyLCIxIjowLCIyIjowfV19LCJoZWlnaHQiOjEwLCJ3aWR0aCI6MTB9"
+		"eyJjb29yZGluYXRlX3RvX2Jsb2NrIjp7IntcInhcIjoxLFwieVwiOjN9Ijp7InR5cGUiOjJ9LCJ7XCJ4XCI6NCxcInlcIjo0fSI6eyJjb2xvciI6MCwidHlwZSI6MH19LCJlZGdlcyI6W10sImV4cGVjdGVkX291dHB1dCI6eyJyb3dzIjpbeyIwIjoxLCIxIjowLCIyIjowfV19LCJoZWlnaHQiOjEwLCJ3aWR0aCI6MTB9"
 	),
 	Level.new(
 		# Still possible to achieve without splitting.
