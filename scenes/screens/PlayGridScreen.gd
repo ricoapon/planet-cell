15f7e9d7a56e46d44e0c7a88fd151bdc131b05c1
Rico Apon <rico.apon@gmail.com>
2025-06-07 13:43:13 +0200

    Implement win and lose popup

--- scenes/screens/PlayGridScreen.gd
+++ scenes/screens/PlayGridScreen.gd
@@ -4,9 +4,13 @@ extends Control
 @onready var GridExecutionViewScene = preload("res://scenes/components/grids/GridExecutionView.tscn")
 @onready var StopButton = $VBoxContainer/ActionsSidePanel/VBoxContainer/MarginContainer/VBoxContainer/StopButton
 @onready var ordered_output_view = $VBoxContainer/ExpectedSidePanel/VBoxContainer/MarginContainer/VBoxContainer/OrderedOutputView
+@onready var won_accept_dialog = $WonAcceptDialog
+@onready var lost_accept_dialog = $LostAcceptDialog
 
 var grid: Grid
 var grid_execution_view: GridExecutionView
+
+signal next_level
 
 func init(_grid: Grid):
 	grid = _grid
@@ -27,13 +31,17 @@ func _on_step_button_pressed() -> void:
 	else:
 		grid_execution_view.next_step()
 		if grid_execution_view.state() == GridExecution.State.WON:
-			print("Won!")
-			# TODO: show win message.
+			won_accept_dialog.popup_centered()
+			
 		elif grid_execution_view.state() == GridExecution.State.LOST:
-			print("Lost :(")
-			# TODO: show lost message.
+			lost_accept_dialog.popup_centered()
 
-func _on_stop_button_pressed() -> void:
+func _on_next_level():
+	next_level.emit()
+
+# This method is used by many signals to stop the grid execution.
+func _on_stop():
+	ordered_output_view.init(null, grid.expected_output)
 	grid_execution_view.queue_free()
 	grid_execution_view = null
 	StopButton.disabled = true
